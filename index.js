cc766883d3a71f367eeb42298e3f691bf97ee25a
Ian Gallgher <igallag93@gmail.com>
2019-12-03 13:45:45 -0500

    added some testing functions to it

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
-const {Client, Attachment} = require('discord.js');
+const { Client, Attachment } = require('discord.js');
 const client = new Client();
 const auth = require('./auth.json');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  
 });
 // this is where the message checker is
 // reads all messages sent in the server to respond
 // should probably add the auth.prefix so it only checks the full message if there is the proper prefix
 client.on('message', msg => {
-  if (msg.content.toLowerCase() === 'ping') {
-    msg.reply('pong');
-  } else if(msg.content === '!code') {
-      msg.channel.send('https://youtu.be/u3CKgkyc7Qo?t=20')
-  } else if(msg.content === 'info') {
-      msg.channel.send('sup')
-  } else if(msg.content.toLowerCase() === 'who is here') {
-      let users = []
-      client.guilds.forEach(async (guild) => {
-        await guild.members.forEach((el) => {
-            users.push(el.displayName)
-        })
-      })
-      msg.channel.send(`${users} are all here!`)
+//   console.log(msg, 'this is message');
+
+  if (msg.author.username !== 'GameTime') {
+    // msg.channel.send(
+    //   `${msg.author.username} sent a message that says '${msg.content}'`
+    // );
+    if (msg.content.toLowerCase() === 'ping') {
+      msg.reply('pong');
+    } else if (msg.content === '!code') {
+      msg.channel.send('https://youtu.be/u3CKgkyc7Qo?t=20');
+    } else if (msg.content === 'info') {
+      msg.channel.send('sup');
+    } else if (msg.content.toLowerCase() === 'who is here') {
+      let users = [];
+      client.guilds.forEach(async guild => {
+        await guild.members.forEach(el => {
+          users.push(el.displayName);
+        });
+      });
+
+    //   let userStr = users.split('');
+      msg.channel.send(`${users} are all here!`);
+    } else if(msg.content.toLowerCase() === 'fuck'){
+        msg.author.send('Hey pal, maybe cool it with the swears')
+    } else if(msg.content.toLowerCase() === 'what game')
   }
 });
 
