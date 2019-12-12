6cc4e79b75689fec19dcb54f4687369b8146eaa8
Ian Gallgher <igallag93@gmail.com>
2019-12-12 14:19:33 -0500

    built the outline of the game store

---
+++ client/store/game.js
@@ -0,0 +1,44 @@
+import axios from 'axios'
+/*
+Action Types
+*/
+const START_TIME = 'START_TIME'
+const TOTAL_TIME = 'TOTAL_TIME'
+
+/*
+Initial State
+*/
+const defaultState = {
+  startTime: 0,
+  totalTime: 0,
+  endTime: 0
+}
+
+/*
+Action Creators
+*/
+const setStartTime = startTime => ({type: START_TIME, startTime})
+const setTotalTime = totalTime => ({type: TOTAL_TIME, totalTime})
+
+/*
+Thunk Creators
+*/
+export const setTotalTimeThunk = () => {
+  return async dispatch => {
+    try {
+      // const {data} = axios.get()
+      console.log(
+        'hey youre in the setTotalTimeThunk and we havent built it yet!'
+      )
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
+export default function(state = defaultState, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
