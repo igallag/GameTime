66b4702fbce64a67a2564f1dd970d92282ddf3cd
Ian Gallgher <igallag93@gmail.com>
2020-01-10 13:51:00 -0500

    removed the end test redundency

--- index.js
+++ index.js
@@ -72,37 +72,6 @@ client.on('message', async msg => {
         {game: gameName}
       )
       msg.author.send(`You subscribed to ${gameName}`)
-    } else if (msg.content.toLowerCase().startsWith('end test')) {
-      console.log('test ended')
-      let gameName = msg.content.slice(8).trim()
-
-      // this is just a temp test and will probably be moved
-      gameName = gameName
-        .replace(/\s/g, '_')
-        .replace(/\W/g, '')
-        .replace(/_/g, '-')
-        .toLowerCase()
-      let currGame = await axios.get(
-        `http://localhost:8080/api/games/${msg.author.id}/${gameName}`
-      )
-      currGame = currGame.data
-
-      let endTime = Date.now()
-      let startTime = Date.parse(currGame.startTime)
-
-      let finalTotal = endTime - startTime
-
-      finalTotal = Math.floor(finalTotal / 60000)
-      let {data} = await axios.put(
-        `http://localhost:8080/api/games/${msg.author.id}/${gameName}`,
-        {total: finalTotal}
-      )
-
-      console.log(
-        `the current session was: ${finalTotal} mins long.  The total time played is ${
-          data.timePlayed
-        }`
-      )
     }
   }
 })
